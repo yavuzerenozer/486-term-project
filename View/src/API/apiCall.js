5deed033a12b38e8cd07b897a6e5b586fc95aef3
Yavuz Özer <49324804+yavuzerenozer@users.noreply.github.com>
2021-11-17 10:48:35 +0300

    facade pattern uploaded

--- View/src/API/apiCall.js
+++ View/src/API/apiCall.js
@@ -1,12 +1,69 @@
 import axios from "axios";
 
-export async function getEarthquakes() {
+async function getEarthquakes0() {
 
-    let array = [];
-    await axios
-    .get('http://localhost:8080/api/')
-    .then((response) => {
-        array = response.data
-    })
-    return array;
+    return fetch('http://localhost:8080/api/', {
+    method: "GET",
+    headers: {"Content-Type": "application/json"}
+  }).then((res) => {
+    
+    res.json()  
+    
+  })
 }
+
+async function getNews0(searchTerm) {
+    return fetch(`http://localhost:8080/news?term=${searchTerm}`, {
+    method: "GET",
+    headers: {"Content-Type": "application/json"}
+  }).then(res => res.json())
+}
+
+
+///////////////////////FACADE APPLIED//////////////////////
+
+
+
+async function getEarthquakes1() {
+    return getFetch1('http://localhost:8080/api/')
+}
+
+async function getNews1(searchTerm) {
+    return getFetch1('http://localhost:8080/news',
+    {searchTerm: searchTerm})
+}
+
+async function getFetch1(url, params = {}) {
+  const query = Object.entries(params).map(param => {
+    return `${param[0]}=${param[1]}`
+  }).join('&')
+
+  return fetch(`${url}?${query}`, {
+    method: "GET",
+    headers: {"Content-Type": "application/json"}
+  }).then(res=>res.json)
+}
+
+//////////////USE CASE: CHANGE OF HTTP CLIENT//////////////
+
+
+
+export async function getEarthquakes(){
+  return getFetch('http://localhost:8080/api/')
+}
+
+export async function getNews(searchTerm) {
+  return getFetch('http://localhost:8080/news', {searchTerm: searchTerm})
+}
+
+async function getFetch(url, params = {}) {
+  return axios ({
+    url:url,
+    method: "GET",
+    params:params
+  }).then(res=>res.data)
+}
+
+
+
+
